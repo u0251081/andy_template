fa2e5ca7824ae9ff87e5e91cb8315ea737e71b1d
AndyChen <u0251081@nkfust.edu.tw>
2018-06-30 09:25:37 +0800

    index add new link

--- index.php
+++ index.php
@@ -2,5 +2,8 @@
 //    echo 'hello world';
 ?>
 <link rel="icon" href="/andy/favicon.ico">
-<a href="demo_php_connect_mysql">demo_php_connect_mysql</a>
-<a href="demo_md5">demo_md5</a>
+<div style="display:flex;flex-direction:column;">
+    <a href="demo_php_connect_mysql">demo_php_connect_mysql</a>
+    <a href="demo_md5">demo_md5</a>
+    <a href="demo_jquery_steps">demo_jquery_steps</a>
+</div>
