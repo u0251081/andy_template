5350046a529fa03dc540b142ab51a1395fb3f3c7
AndyChen <u0251081@nkfust.edu.tw>
2018-07-10 23:21:57 +0800

    fix connection

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 ?>
 <link rel="icon" href="/andy/favicon.ico">
 <div style="display:flex;flex-direction:column;">
-    <a href="demo_php_connect_mysql">demo_php_connect_mysql</a>
+    <a href="demo_php_connect_mysql/sample_connect.php">demo_php_connect_mysql</a>
     <a href="demo_md5">demo_md5</a>
     <a href="demo_jquery_steps">demo_jquery_steps</a>
 </div>
