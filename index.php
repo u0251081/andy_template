478af5f80384bc9dcf26e7a40d27f2df8487cfff
AndyChen <u0251081@nkfust.edu.tw>
2018-07-12 22:16:49 +0800

    add demo of php of OOP

--- index.php
+++ index.php
@@ -6,4 +6,5 @@
     <a href="demo_php_connect_mysql/sample_connect.php">demo_php_connect_mysql</a>
     <a href="demo_md5">demo_md5</a>
     <a href="demo_jquery_steps">demo_jquery_steps</a>
+    <a href="demo_php_OOP">demo_php_OOP</a>
 </div>
